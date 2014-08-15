d1e37220e054d4edae68e2915e8e9ebfca905738
Stuart P. Bentley <stuart@testtrack4.com>
2014-08-15 18:42:15 +0000

    Initial content

---
+++ server.js
@@ -0,0 +1,27 @@
+var queue = require('queue-async');
+var express = require('express');
+var request = require('request');
+
+var app = express();
+app.use(express.static(__dirname + '/static'));
+
+app.get('/:owner/:repo', function(req, res, next) {
+  var q = queue();
+  var repoName = req.params.owner + '/' + req.params.repo;
+  res.locals.repoName = repoName;
+  for (var i=1; i<=10; i++) {
+    q.defer(request,{
+      url:'https://api.github.com/repos/' + repoName + '/events',
+      qs: {page: i},
+      json: true
+      });
+  }
+  q.awaitAll(function(err, pages) {
+    if (err) return next(err);
+    res.locals.stargazes = [].concat.apply([], pages).filter(function(v) {
+      return v.type == 'WatchEvent';
+    });
+    res.set('content-type','text/html');
+    res.render(__dirname + 'starwatchcats.jade');
+  });
+});
